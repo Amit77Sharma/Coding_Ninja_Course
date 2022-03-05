3ca3374d07a562679072be658df853a004fc5f17
Amit Kumar Sharma <amitmzc77@gmail.com>
2022-03-05 12:19:48 +0530

    git commit -u"practice_code"

---
+++ practice/song.java
@@ -0,0 +1,31 @@
+package practice;
+//package practice;
+//
+//public class Song {
+//		private int songId;
+//		private String title;
+//		private String artist;
+//		private double duration;
+//		
+//		public Song(int songId, String title, String artist, double duration) {
+//			this.songId=songId;
+//			this.title=title;
+//			this.artist=artist;
+//			this.duration=duration;
+//		}
+//		
+//		public int getsongId() {
+//			return songId;
+//		}
+//		
+//		public String gettitle() {
+//			return title;
+//		}
+//		
+//		public String getartist() {
+//			return artist;
+//		}
+//		public double getduration() {
+//			return duration;
+//		}
+//}
